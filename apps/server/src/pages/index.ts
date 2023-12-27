f2e086cfaab5ecff5f3a78e371f578280f6b61a5
Zioth <louisgiet.w@gmail.com>
2023-12-27 21:33:22 +0100

    chore(server): removed unused dependency

--- apps/server/src/pages/index.ts
+++ apps/server/src/pages/index.ts
@@ -1,11 +1,3 @@
-import { superjson } from "@acme/api/client";
-
 export default () => {
-  fetch("http://localhost:3000/api/trpc/user.test", {
-    method: 'POST',
-  body: JSON.stringify({
-    hello: 'world'
-  })
-  })
   return null;
 }
